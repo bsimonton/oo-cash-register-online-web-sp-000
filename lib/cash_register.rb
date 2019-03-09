6bc545c9ff7313ef06bb0e94b367382be3ac26f5
Brian Simonton <bsimonton78@gmail.com>
2019-03-09 02:53:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,7 +22,15 @@ def add_item(item, price, quantity = 1)
     end
   end
 
-
+def apply_discount
+    if @discount > 0
+      @to_take_off = (price * discount)/100
+      @total -= @to_take_off
+      return "After the discount, the total comes to $#{total}."
+    else
+      return "There is no discount to apply."
+    end
+  end
 
 
 def void_last_transaction
